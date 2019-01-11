c543f6848055e4cd17df662a1ab6b1943ca07f15
adarsh.bajpai <adarsh.bajpai@lampiris.be>
2019-01-11 23:48:24 +0530

    CurrencyConversion

---
+++ src/main/java/com/springboot/microservice/example/currencyconversion/Bean/CurrencyConversionBean.java
@@ -0,0 +1,60 @@
+package com.springboot.microservice.example.currencyconversion.Bean;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by adbajpai on 1/8/2019.
+ */
+public class CurrencyConversionBean {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+    private BigDecimal quantity;
+    private BigDecimal totalCalculatedAmount;
+    private int port;
+
+    public CurrencyConversionBean() {
+
+    }
+
+    public CurrencyConversionBean(Long id, String from, String to, BigDecimal conversionMultiple, BigDecimal quantity,
+                                  BigDecimal totalCalculatedAmount, int port) {
+        super();
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+        this.quantity = quantity;
+        this.totalCalculatedAmount = totalCalculatedAmount;
+        this.port = port;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return conversionMultiple;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public BigDecimal getTotalCalculatedAmount() {
+        return totalCalculatedAmount;
+    }
+
+    public int getPort() {
+        return port;
+    }
+}
---
+++ src/main/java/com/springboot/microservice/example/currencyconversion/controller/CurrencyConversionController.java
@@ -0,0 +1,36 @@
+package com.springboot.microservice.example.currencyconversion.controller;
+
+import com.springboot.microservice.example.currencyconversion.Bean.CurrencyConversionBean;
+import com.springboot.microservice.example.currencyconversion.proxy.CurrencyExchangeServiceProxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by adbajpai on 1/8/2019.
+ */
+@RestController
+public class CurrencyConversionController {
+
+    @Autowired
+    private CurrencyExchangeServiceProxy proxy;
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
+                                                  @PathVariable BigDecimal quantity) {
+
+        CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+
+
+        return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
+                quantity.multiply(response.getConversionMultiple()), response.getPort());
+    }
+
+}
